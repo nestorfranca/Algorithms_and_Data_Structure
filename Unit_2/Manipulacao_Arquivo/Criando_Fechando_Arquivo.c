7bff6a1116338d72d34a634608a5f1056e326192
Nestor França de Andrade júnior <138056526+nestorfranca@users.noreply.github.com>
2023-08-01 16:51:34 -0300

    Update and rename Criando_Arquivo.c to Criando_Fechando_Arquivo.c

---
+++ Unit_2/Manipulacao_Arquivo/Criando_Fechando_Arquivo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+
+    FILE *fp;
+    fp = fopen("entrada.txt", "wt");  //Criar arquivo
+    if(fp == NULL){
+        printf("Erro na abertura do arquivo!\n");
+        exit(1);
+    }else{ printf("Arquivo criado!\n ");}
+    
+    if(!fclose(fp)){    //Fechar arquivo
+        printf("Arquivo fechado!\n");
+    } 
+
+    return 0;
+}
