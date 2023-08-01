df72e614200b3e009878de6fc765e65bf7ee35ff
Nestor França de Andrade júnior <138056526+nestorfranca@users.noreply.github.com>
2023-08-01 16:47:28 -0300

    Create Criando_Arquivo.c

---
+++ Unit_2/Manipulacao_Arquivo/Criando_Arquivo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+
+    FILE *fp;
+    fp = fopen("entrada.txt", "wt");
+    if(fp == NULL){
+        printf("Erro na abertura do arquivo!");
+        exit(1);
+    }
+
+    return 0;
+}
