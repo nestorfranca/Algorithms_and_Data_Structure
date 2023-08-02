1a15ee40a22b894b20926be4b36c6f2c5ced6727
Nestor França de Andrade júnior <138056526+nestorfranca@users.noreply.github.com>
2023-08-02 16:38:06 -0300

    Create String.c

---
+++ Unit_2/Manipulacao_Arquivo/String.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void){
+
+    char nome_arquivo[20];
+    int c;
+     
+    printf("Informe o nome do arquivo: \n");
+    scanf(" %[^\n]", nome_arquivo);     //RECEBE O NOME DO ARQUIVO
+    char ext[] = ".txt";        //CONCATENA ".TXT" NO FINAL
+    strcat(nome_arquivo, ext);
+
+    FILE * arquivo = fopen(nome_arquivo, "w");   //ABRI ARQUIVO 
+    if(arquivo == NULL){
+        printf("Erro ao abrir o arquivo\n");
+        return 1;
+    }else {
+        printf("Aquivo criado com sucesso\n");
+    }
+    fputs("Hello World!", arquivo);       //ESCREVE 'string' NO ARQUIVO CRIADO
+    
+    fclose(arquivo);  //FECHA ARQUIVO
+
+    return 0;
+}
