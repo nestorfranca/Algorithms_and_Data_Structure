368bb3b837419f3c0941c21b3ab467d4de4f974f
Nestor França de Andrade júnior <138056526+nestorfranca@users.noreply.github.com>
2023-08-09 17:25:08 -0300

    Create str.h

---
+++ Unit_2/Modulo_Compilacao/str.h
@@ -0,0 +1,16 @@
+/*Funcoes oferecidas pelo modulo str.c */
+
+/*Funcao comprimento
+**Retorna o numero de caracteres da string passada como parametro
+*/
+int comprimento (char *str);
+
+/*Funcao copia
+**Copia os caracteres da string oring (origem) para dest (destino)
+*/
+void copia (char *dest, char *orig);
+
+/*Funcao concatena
+**Concatena a string orig (origem) na string dest (destino)
+*/
+void concatena (char *dest, char *orig);
