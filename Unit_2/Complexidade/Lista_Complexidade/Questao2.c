fcb540028c4030659ea3ef236701f16cc192d71a
Nestor França de Andrade júnior <138056526+nestorfranca@users.noreply.github.com>
2023-08-23 17:21:16 -0300

    Create Questao2.c

---
+++ Unit_2/Complexidade/Lista_Complexidade/Questao2.c
@@ -0,0 +1,21 @@
+função:
+        c1*n
+        c2*n
+      	c3
+n*(c1 + c2) + c3  =>  an + b
+
+
+main: 
+	c4
+	c5
+	c7
+	função = an + b
+	c8
+	c9
+	c10
+
+
+Tudo:
+	c = c3 + c4 + c5 + c6 + c7 + c8 + c9 + c10
+	an + c
+	
